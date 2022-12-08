d76dff372ef38c9eeaa3e30fae544757d3e6a94a
jort2 <>
2022-12-08 21:45:47 +0100

    Frontend installation

--- server/index.js
+++ server/index.js
@@ -26,3 +26,15 @@ app.use("/client", clientRoutes);
 app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
+
+/* Mongoose setup */
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error) => console.log(`${error} did not connect`));
