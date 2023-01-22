67d6cf35a79180efc9ba890d9fde8d1e309d0e4a
jort2 <>
2023-01-22 17:28:45 +0100

    Created Customers page

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,19 @@ import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { themeSettings } from "theme"
+import { themeSettings } from "theme";
 import Layout from "scenes/layout";
-import Dashboard from 'scenes/dashboard';
+import Dashboard from "scenes/dashboard";
+import Products from "scenes/products";
+import Customers from "scenes/customers";
+import Transactions from "scenes/transactions";
+/* import Geography from "scenes/geography";
+import Overview from "scenes/overview";
+import Daily from "scenes/daily";
+import Monthly from "scenes/monthly";
+import Breakdown from "scenes/breakdown";
+import Admin from "scenes/admin";
+import Performance from "scenes/performance";  */ 
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -14,18 +24,21 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Routes>
-          <Route element={<Layout/>}>
-            <Route path="/" element={<Navigate to="/dashboard" replace />}/>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-        </Routes>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+            
+            </Route>
+          </Routes>
         </ThemeProvider>
       </BrowserRouter>
-  </div>
+    </div>
   );
 }
-
 
 export default App;
