c783b12126e203c70b5581d6a6da3e324c862c48
jort2 <>
2023-01-27 21:12:38 +0100

    Created overview page

--- server/index.js
+++ server/index.js
@@ -15,9 +15,8 @@ import User from "./models/User.js";
 import Product from './models/Product.js';
 import ProductStat from "./models/ProductStat.js";
 import Transaction from "./models/Transaction.js";
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from './data/index.js';
-
-
+import OverallStat from './models/OverallStat.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from './data/index.js';
 
 /* Configuration */
 dotenv.config();
@@ -47,9 +46,10 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ONLY ADD DATA ONE TIME*/
-     //  Product.insertMany(dataProduct);
-     //  ProductStat.insertMany(dataProductStat);
-     //  Transaction.insertMany(dataTransaction);
-    // User.insertMany(dataUser);
+    //OverallStat.insertMany(dataOverallStat);
+     // Product.insertMany(dataProduct);
+     // ProductStat.insertMany(dataProductStat);
+     // Transaction.insertMany(dataTransaction);
+     // User.insertMany(dataUser);
 })
 .catch((error) => console.log(`${error} did not connect`));
